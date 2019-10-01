634cd5d61119eed5c46dce2d26b9ce52476f6f8b
Mahamud Babu <mahmudbabu94@gmail.com>
2019-10-01 13:40:03 -0400

    Hello world test

---
+++ Auditive/HelloWorld.m
@@ -0,0 +1,13 @@
+//
+//  HelloWorld.m
+//  Auditive
+//
+//  Created by admin on 9/30/19.
+//  Copyright © 2019 Semasiology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+oscillator1 = AKOscillator();
+oscillator2 = AKOscillator();
+mixer = AKMixer();
+
