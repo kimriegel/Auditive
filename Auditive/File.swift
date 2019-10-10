e3d56ffd9d52cff8bda7eae954d04945669dcc86
Mahamud Babu <mahmudbabu94@gmail.com>
2019-10-10 01:01:40 -0400

    This will be the building blocks for the AMP and FREquency functions

---
+++ Auditive/File.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Auditive
+//
+//  Created by admin on 10/9/19.
+//  Copyright © 2019 Semasiology. All rights reserved.
+//
+
+import Foundation
+import AudioKitPlaygrounds
+import AudioKit
+
+
+
---
+++ Auditive/Helloworld_Amp.swift
@@ -0,0 +1,17 @@
+//
+//  Helloworld_Amp.swift
+//  Auditive
+//
+//  Created by admin on 10/9/19.
+//  Copyright © 2019 Semasiology. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVKit
+import AVFoundation
+import os
+
+import CloudKit
+
+let str = print("Hello World")
